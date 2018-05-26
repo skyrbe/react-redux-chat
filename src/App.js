831696ca68a848bbc92936c2e94322c3988096d4
skyrbe <harsha.venkatram@sakhatech.com>
2018-05-26 20:18:42 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TestComponent        from 'components/TestComponent'
 import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
