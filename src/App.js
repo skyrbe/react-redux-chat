16ea800ba4b2bfa585c8df60bf1521e762ef565a
skyrbe <harsha.venkatram@sakhatech.com>
2018-05-31 14:37:54 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import TestComponent        from 'components/TestComponent'
+import Chat from 'containers/Chat';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <TestComponent>
-        1
-      </TestComponent>
+      <Chat />
     )
   }
 }
